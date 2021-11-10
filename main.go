8d68a98649af949ac528267e73ea93d35fce73f3
Brendan Prednis <brendan.prednis@protonmail.com>
2021-11-10 13:36:05 -0800

    added basic setup for reading data.json in init func.
    Signed-off-by: Brendan Prednis <brendan.prednis@protonmail.com>

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 )
 
+type recipeData struct {
+	Name         string            `json:"name"`
+	Ingredients  map[string]string `json:"ingredients"`
+	Instructions map[string]string `json:"instructions"`
+}
+
+var DataFile map[string]recipeData
+
+// init will load our json file into RecipeData before anything else runs.
 func init() {
 	// "db" should be loaded here, before anything else.
-	_, err := ioutil.ReadFile("data.json")
+	content, err := ioutil.ReadFile("data.json")
 	if err != nil {
 		log.Fatal("could not read data.json, application will not work.")
+	}
+
+	err = json.Unmarshal(content, &DataFile)
+	if err != nil {
+		log.Fatal("error: could not Unmarshal json to recipeData: ", err)
 	}
 }
 
